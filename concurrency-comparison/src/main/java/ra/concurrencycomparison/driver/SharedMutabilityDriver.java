1a3e9ab8aac2ffe1f8a29144cae5d5bfbfeba735
Remus Amalinei <remus.amalinei@gmail.com>
2015-10-21 17:38:53 +0100

    Refactor SharedMutabilityDriver
    Project: concurrency-comparison

--- concurrency-comparison/src/main/java/ra/concurrencycomparison/driver/SharedMutabilityDriver.java
+++ concurrency-comparison/src/main/java/ra/concurrencycomparison/driver/SharedMutabilityDriver.java
@@ -1,12 +1,8 @@
 package ra.concurrencycomparison.driver;
 
-import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Map;
 
 import ra.concurrencycomparison.Adder;
-import ra.concurrencycomparison.Constants;
 import ra.concurrencycomparison.SharedMutabilityAdder;
 
 /**
@@ -17,46 +13,17 @@ public class SharedMutabilityDriver {
     public static void main(String[] args) throws InterruptedException {
         int producerThreadCount = Integer.parseInt(args[0]);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(producerThreadCount);
-        CountDownLatch countDownLatch = new CountDownLatch(producerThreadCount);
-
-        int sliceSize = Constants.WORD_TO_GENERATE_COUNT / producerThreadCount;
+        long start = System.currentTimeMillis();
 
         Adder adder = new SharedMutabilityAdder();
-        int mapSize = Constants.TEN_WORD_MAP.size();
-        for (int i = 0; i < producerThreadCount; i++) {
-            final int from = i * sliceSize;
-            final int to = calculateTo(i, producerThreadCount, sliceSize);
 
-            Runnable task = () -> {
-                Random random = new Random();
-                for (int j = from; j < to; j++) {
-                    int randomKey = random.nextInt(mapSize);
-                    String word = Constants.TEN_WORD_MAP.get(randomKey);
+        DriverUtils.addConcurrently(adder, producerThreadCount);
 
-                    adder.add(word);
-                }
-                countDownLatch.countDown();
-            };
+        Map<String, Integer> result = adder.computeSum();
 
-            executorService.execute(task);
-        }
+        long duration = System.currentTimeMillis() - start;
 
-        executorService.shutdown();
-        countDownLatch.await();
-
-        adder.computeSum();
-    }
-
-    private static int calculateTo(int currentStep, int finalExclusiveStep, int sliceSize) {
-        if ( ! lastStep(currentStep, finalExclusiveStep)) {
-            return (currentStep + 1) * sliceSize;
-        } else {
-            return Constants.WORD_TO_GENERATE_COUNT;
-        }
-    }
-
-    private static boolean lastStep(int currentStep, int finalExclusiveStep) {
-        return currentStep == (finalExclusiveStep - 1);
+        System.out.println("[SharedMutabilityDriver] " + duration + " ms");
+        System.out.println("[SharedMutabilityDriver] " + result);
     }
 }
